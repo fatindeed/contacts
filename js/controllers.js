38f82b08dbeaf4b35dbcd68c00c75c808bce13a8
James Zhu <fatindeed@hotmail.com>
2014-02-18 16:07:02 +0800

    alpha

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,29 @@
 angular.module('AppControllers', [])
 .controller('ContactListCtrl', function($scope) {
-	ionic.Platform.ready(function(){
+	$scope.isLoading = true;
+	ionic.Platform.ready(function() {
 		var options = new ContactFindOptions();
 		options.filter = '';
 		options.multiple = true;
 		var fields = ['*']
 		navigator.contacts.find(fields, function(contacts) {
 			$scope.$apply(function() {
+				for(var i = 0; i < contacts.length; i++) {
+					if(contacts[i].displayName != null) {
+						contacts[i].photoImage = contacts[i].photos[0].value;
+					}
+					else {
+						contacts[i].photoImage = 'img/default-photo.png';
+					}
+					if(contacts[i].displayName == null) {
+						if(contacts[i].name != null) {
+							contacts[i].displayName = contacts[i].name.familyName + contacts[i].name.givenName;
+						}
+						else {
+							contacts[i].displayName = '- No Name -';
+						}
+					}
+				}
 				$scope.contacts = contacts;
 			});
 		}, function(error) {
@@ -14,6 +31,7 @@ angular.module('AppControllers', [])
 				$scope.error = error;
 			});
 		}, options);
+		$scope.isLoading = false;
 	});
 })
 .controller('ContactDetailCtrl', function($scope, $stateParams, Contacts) {
